424d19d1ab98a7dc85b991af3f39fcfa35871609
protyze <jan.azzati@gmail.com>
2019-03-20 19:17:19 +0100

    Placeholders for filter form and team-list, wired up employees service with components and implemented an observer

--- frontend/src/app/team-generator/team-generator.component.ts
+++ frontend/src/app/team-generator/team-generator.component.ts
@@ -1,18 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import {EmployeeService} from '../services/employee.service';
+import { Filter } from '../domain/Filter';
 
+import { EmployeeService } from '../services/employee.service';
+import { Team } from '../domain/Team';
 
 @Component({
   selector: 'app-team-generator',
   templateUrl: './team-generator.component.html',
   styleUrls: ['./team-generator.component.scss']
 })
-export class TeamGeneratorComponent implements OnInit {
+export class TeamGeneratorComponent implements OnInit, OnDestroy {
+  private filters: Filter;
+  private teamResults: Array<Team> = [];
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private swEmployee: EmployeeService) { }
 
   ngOnInit() {
+    this.swEmployee.teamsFetched.subscribe(() => {
+      this.getTeams();
+    });
   }
 
+  onFilterChanged(filters: Filter) {
+    this.filters = filters;
+    // TODO: API Request
+    this.swEmployee.fetchTeams(filters);
+  }
+
+  getTeams() {
+    this.teamResults = this.swEmployee.getTeams();
+  }
+
+  ngOnDestroy() {
+    this.swEmployee.teamsFetched.unsubscribe();
+  }
 }
