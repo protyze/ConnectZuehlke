eff1c8b334b3573623e8a33943d1a75381ab9ac6
protyze <jan.azzati@gmail.com>
2019-03-20 18:38:27 +0100

    Remove unnecessary components, create routes and initial team-generator component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,7 +8,6 @@ import {faHeart} from '@fortawesome/free-solid-svg-icons';
 import {faAngular, faJava} from '@fortawesome/free-brands-svg-icons';
 import {intersectionObserverPreset, LazyLoadImageModule} from 'ng-lazyload-image';
 
-
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {EmployeeListComponent} from './employee-list/employee-list.component';
 import {EmployeeComponent} from './employee-list/employee/employee.component';
@@ -29,22 +28,17 @@ import {
 } from '@angular/material';
 import {NavigationComponent} from './navigation/navigation.component';
 import {LayoutModule} from '@angular/cdk/layout';
-import {HackathonHeadlineComponent} from './hackathon-headline/hackathon-headline.component';
 import {SidebarComponent} from './navigation/sidebar/sidebar.component';
 import {TopbarComponent} from './navigation/topbar/topbar.component';
 import {EmployeeDetailComponent} from './employee-detail/employee-detail.component';
-import {MapComponent} from './map/map.component';
+import {TeamGeneratorComponent} from './team-generator/team-generator.component';
 import {AgmCoreModule} from '@agm/core';
-import {CustomerListComponent} from './customer-list/customer-list.component';
-import {CustomerComponent} from './customer-list/customer/customer.component';
 import {ErrorRequestInterceptor} from './common/error-request-interceptor';
-
 
 // For more icons, please checkout https://fontawesome.com/icons?d=gallery
 library.add(faHeart);
 library.add(faJava);
 library.add(faAngular);
-
 
 @NgModule({
   declarations: [
@@ -54,13 +48,10 @@ library.add(faAngular);
     PageNotFoundComponent,
     SearchPipe,
     NavigationComponent,
-    HackathonHeadlineComponent,
     SidebarComponent,
     TopbarComponent,
     EmployeeDetailComponent,
-    MapComponent,
-    CustomerListComponent,
-    CustomerComponent
+    TeamGeneratorComponent
   ],
   imports: [
     BrowserModule,
